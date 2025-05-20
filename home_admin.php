a81a0984e0091ef06b08d19e222f689b880cc937
AminSolehudin <2203010110@unper.ac.id>
2025-05-20 18:53:34 +0700

    Create Home Admin UI

---
+++ home_admin.php
@@ -0,0 +1,68 @@
+<marquee>Selamat Datang Dihalaman utama Anda Login Sebagai ADMIN </marquee>
+<div id="page-inner">
+    <div class="row">
+        <div class="col-md-12">
+            <h3>Halaman Admin</h3>
+            <h5></h5>
+        </div>
+    </div>
+    <!-- /. ROW  -->
+    <hr />
+
+
+    <div class="row">
+        <!-- Data Anggota -->
+        <div class="col-md-4 col-sm-12 col-xs-12">
+            <a href="?page=anggota">
+                <div class="panel panel-primary text-center no-boder bg-color-green">
+                    <div class="panel-body">
+                        <i class="fa fa-users fa-5x"></i>
+                    </div>
+
+                    Data Anggota
+
+                </div>
+            </a>
+        </div>
+
+        <!-- Data Buku -->
+        <div class="col-md-4 col-sm-12 col-xs-12">
+            <a href="?page=buku">
+                <div class="panel panel-primary text-center no-boder bg-color-red">
+                    <div class="panel-body">
+                        <i class="fa fa-book fa-5x"></i>
+                    </div>
+
+                    Data Buku
+
+                </div>
+            </a>
+        </div>
+
+        <!-- Transaksi -->
+        <div class="col-md-4 col-sm-12 col-xs-12">
+            <a href="?page=transaksi">
+                <div class="panel panel-primary text-center no-boder bg-color-blue">
+                    <div class="panel-body">
+                        <i class="fa fa-exchange fa-5x"></i>
+                    </div>
+
+                    Transaksi
+                </div>
+            </a>
+        </div>
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <a href="?page=buku">
+                <div class="panel panel-primary text-center no-boder bg-color-yellow">
+                    <div class="panel-body">
+                        <i class="fa fa-user fa-5x"></i>
+                    </div>
+                    <div class="panel-footer back-footer-blue">
+                        Data Pengguna
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+</div>
