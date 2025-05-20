efb78de09bb040eb80a624bd00d8b594944b3694
AminSolehudin <2203010110@unper.ac.id>
2025-05-20 18:51:11 +0700

    Create Home.php

---
+++ home.php
@@ -0,0 +1,57 @@
+<marquee>Selamat Datang Dihalaman utama Anda Login Sebagai Petugas </marquee>
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     <h3>Halaman Petugas</h3>   
+                        <h5></h5>
+                    </div>
+                </div>              
+                 <!-- /. ROW  -->
+                  <hr />
+
+                 
+                  <div class="row">
+    <!-- Data Anggota -->
+    <div class="col-md-4 col-sm-12 col-xs-12">
+        <a href="?page=anggota">                        
+            <div class="panel panel-primary text-center no-boder bg-color-green">
+                <div class="panel-body">
+                    <i class="fa fa-users fa-5x"></i>
+                </div>
+             
+                    Data Anggota
+                
+            </div>
+        </a>
+    </div>
+
+    <!-- Data Buku -->
+    <div class="col-md-4 col-sm-12 col-xs-12">
+        <a href="?page=buku">                        
+            <div class="panel panel-primary text-center no-boder bg-color-red">
+                <div class="panel-body">
+                    <i class="fa fa-book fa-5x"></i>
+                </div>
+            
+                    Data Buku
+                
+            </div>
+        </a>
+    </div>
+
+    <!-- Transaksi -->
+    <div class="col-md-4 col-sm-12 col-xs-12">
+        <a href="?page=transaksi">                        
+            <div class="panel panel-primary text-center no-boder bg-color-blue">
+                <div class="panel-body">
+                    <i class="fa fa-exchange fa-5x"></i>
+                </div>
+                
+                    Transaksi
+                
+            </div>
+        </a>
+    </div>
+</div>
+
+			</div>
