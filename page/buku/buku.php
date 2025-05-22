bb51648feeec4bc6c146a98c5581a40f7bcea3db
AminSolehudin <2203010110@unper.ac.id>
2025-05-22 14:08:37 +0700

    Create UI Page Buku

---
+++ page/buku/buku.php
@@ -0,0 +1,42 @@
+<a href="" class="btn btn-primary" style="margin-bottom: 5px">Tambah Data</a>
+<div class="row">
+    <div class="col-md-12">
+        <!-- Advanced Tables -->
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                Data Buku
+            </div>
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Judul</th>
+                                <th>Pengarang</th>
+                                <th>Penerbit</th>
+                                <th>ISBN</th>
+                                <th>Jumlah Buku</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                    <a href="" class="btn btn-info"><i class="fa fa-edit"></i>Ubah</a>
+                                    <a onclick="" href="" class="btn btn-danger"><i class="fa fa-trash"></i>Hapus</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
