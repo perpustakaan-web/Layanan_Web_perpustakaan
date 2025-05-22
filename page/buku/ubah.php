d020f9e25fac07e901d9a9421f0f1a5ca3276c63
Nauval0511 <nauvalms152@gmail.com>
2025-05-22 17:12:01 +0700

    Fetch data and Logic on the Ubah Buku Page

--- page/buku/ubah.php
+++ page/buku/ubah.php
@@ -1,3 +1,16 @@
+
+<?php 
+    $id = $_GET['id'];
+
+    $sql = $koneksi->query("select * from buku where id='$id'");
+
+    $tampil = $sql->fetch_assoc();
+
+    $tahun2 = $tampil['tahun_terbit'];
+
+    $lokasi = $tampil['lokasi'];
+?>
+
 <div class="panel panel-primary">
     <div class="panel-heading">
         Ubah Data
@@ -9,45 +22,61 @@
                 <form method="POST">
                     <div class="form-group">
                         <label>Judul</label>
-                        <input class="form-control" name="judul" />
+                        <input class="form-control" name="judul" value="<?php echo $tampil['judul']?>" />
 
                     </div>
                     <div class="form-group">
                         <label>Pengarang</label>
-                        <input class="form-control" name="pengarang" />
+                        <input class="form-control" name="pengarang" value="<?php echo $tampil['pengarang']?>" />
 
                     </div>
                     <div class="form-group">
                         <label>Penerbit</label>
-                        <input class="form-control" name="penerbit" />
+                        <input class="form-control" name="penerbit" value="<?php echo $tampil['penerbit']?>" />
 
                     </div>
                     <div class="form-group">
                         <label>Tahun Terbit</label>
                         <select class="form-control" name="tahun_terbit">
+                            <?php
+
+                                $tahun = date("Y");
+
+                                for ($i=$tahun-30; $i <= $tahun ; $i++) {
+                                
+                                        if ($i==$tahun2) {
+                                        echo'<option value="'.$i.'" selected>'.$i.'</option>';
+                                }else{
+                                        echo'<option value="'.$i.'">'.$i.'</option>';
+                                }
+                                    
+
+                                }
+
+                            ?>
                         </select>
                     </div>
                     <div class="form-group">
                         <label>ISBN</label>
-                        <input class="form-control" name="isbn" />
+                        <input class="form-control" name="isbn" value="<?php echo $tampil['isbn']?>" />
 
                     </div>
                     <div class="form-group">
                         <label>Jumlah Buku</label>
-                        <input class="form-control" type="number" name="jumlah_buku" />
+                        <input class="form-control" type="number" name="jumlah_buku" value="<?php echo $tampil['jumlah_buku']?>" />
 
                     </div>
                     <div class="form-group">
                         <label>Lokasi</label>
                         <select class="form-control" name="lokasi">
-                            <option value="rak1">Rak 1</option>
-                            <option value="rak2">Rak 2</option>
-                            <option value="rak3">Rak 3</option>
+                            <option value="rak1" <?php if ($tampil['lokasi']=='rak1') {echo "selected";}?>>Rak 1</option>
+                            <option value="rak2" <?php if ($tampil['lokasi']=='rak2') {echo "selected";}?>>Rak 2</option>
+                            <option value="rak3" <?php if ($tampil['lokasi']=='rak3') {echo "selected";}?>>Rak 3</option>
                         </select>
                     </div>
                     <div class="form-group">
                         <label>Tanggal Input</label>
-                        <input class="form-control" type="date" name="tgl_input" />
+                        <input class="form-control" type="date" name="tgl_input" value="<?php echo $tampil['tgl_input']?>"/>
 
                     </div>
 
@@ -61,3 +90,34 @@
     </div>
 </div>
 </div>
+
+<?php
+
+    $judul =        @$_POST ['judul'];
+    $pengarang =    @$_POST ['pengarang'];
+    $penerbit =     @$_POST ['penerbit'];
+    $tahun_terbit = @$_POST ['tahun_terbit'];
+    $isbn =         @$_POST ['isbn'];
+    $jumlah_buku =  @$_POST ['jumlah_buku'];
+    $lokasi =       @$_POST ['lokasi'];
+    $tgl_input =    @$_POST ['tgl_input'];
+
+    $simpan =       @$_POST ['simpan'];
+
+    if ($simpan) {
+        $sql = $koneksi->query("update buku set judul='$judul',pengarang='$pengarang',penerbit='$penerbit',tahun_terbit='$tahun_terbit',isbn='$isbn',jumlah_buku='$jumlah_buku',lokasi='$lokasi',tgl_input='$tgl_input'
+        where id='$id'");
+       
+
+        if ($sql) {
+            ?>
+                <script type="text/javascript">
+                    alert ("Data Berhasil Diupdate");
+                    window.location.href="?page=buku";
+
+                </script>
+                <?php
+        }
+    }
+
+?>
