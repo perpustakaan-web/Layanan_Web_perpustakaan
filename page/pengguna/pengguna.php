ea068349c5a6a3162be9c9aac202dc1ebee1c15c
AminSolehudin <2203010110@unper.ac.id>
2025-05-24 12:14:04 +0700

    Set Up UI Pengguna

---
+++ page/pengguna/pengguna.php
@@ -0,0 +1,52 @@
+<div class="row">
+    <div class="col-md-12">
+        <!-- Advanced Tables -->
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                Data Pengguna
+            </div>
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Username</th>
+                                <th>Password</th>
+                                <th>Nama</th>
+                                <th>Level</th>
+                                <th>Foto</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>></td>
+                                <td>
+                                    <img src="" alt="Foto" width="100">
+                                </td>
+                                <td>
+                                    <a href="" class="btn btn-info">
+                                        <i class="fa fa-edit"></i> Ubah
+                                    </a>
+                                    <a onclick="" href="" class="btn btn-danger">
+                                        <i class="fa fa-trash"></i> Hapus
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <a href="" class="btn btn-primary" style="margin-top: 8px">
+                    <i class="fa fa-plus"></i> Tambah Data
+                </a>
+
+
+            </div>
+        </div>
+    </div>
+</div>
