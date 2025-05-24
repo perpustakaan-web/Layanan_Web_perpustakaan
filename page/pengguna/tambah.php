269729139e7e61b7c467d996e996a74095c86cf3
AminSolehudin <2203010110@unper.ac.id>
2025-05-24 12:23:55 +0700

    Set Up UI Tambah Pengguna

---
+++ page/pengguna/tambah.php
@@ -0,0 +1,44 @@
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        Tambah Data
+    </div>
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-md-12">
+                <form method="POST">
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input class="form-control" />
+
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input class="form-control" />
+
+                    </div>
+                    <div class="form-group">
+                        <label>nama</label>
+                        <input class="form-control" />
+
+                    </div>
+                    <div class="form-group">
+                        <label>level</label>
+                        <input class="form-control" />
+
+                    </div>
+                    <div class="form-group">
+                        <label>foto</label>
+                        <input class="form-control" type="file" type />
+
+                    </div>
+
+                    <div>
+                        <input type="submit" value="Simpan" class="btn btn-primary">
+                    </div>
+
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
