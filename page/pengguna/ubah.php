70fa948b745a5d0b60c9aedc081313748dca4b37
AminSolehudin <2203010110@unper.ac.id>
2025-05-24 12:30:36 +0700

    Create UI Ubah Pengguna

---
+++ page/pengguna/ubah.php
@@ -0,0 +1,55 @@
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        Ubah Pengguna
+    </div>
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-md-12">
+                <form method="POST">
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input class="form-control" name="" value="" />
+
+                    </div>
+                    <div class="form-group">
+                        <label>Nama</label>
+                        <input class="form-control" name="" value="" />
+
+                    </div>
+                    <div class="form-group">
+                        <label>Tempat Lahir</label>
+                        <input class="form-control" name="" value="" />
+
+                    </div>
+                    <div class="form-group">
+                        <label>Tanggal Lahir</label>
+                        <input class="form-control" type="date" type name="" value="" />
+
+                    </div>
+                    <div class="form-group">
+                        <label>Jenis Kelamin</label> <br />
+                        <label class="checkbox-inline">
+                            <input type="checkbox" value="1" name="" /> Laki-Laki
+                        </label>
+                        <label class="checkbox-inline">
+                            <input type="checkbox" value="2" name="" /> Perempuan
+                        </label>
+
+                    </div>
+                    <div class="form-group">
+                        <label>Prodi</label>
+                        <input class="form-control" name="" value="" />
+
+                    </div>
+
+
+                    <div>
+                        <input type="submit" name="" value="" class="btn btn-primary">
+                    </div>
+
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
