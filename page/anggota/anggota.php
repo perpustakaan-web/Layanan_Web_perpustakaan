4b78615ddc6ca4fca57d2708f3d395c1eda700a5
AminSolehudin <2203010110@unper.ac.id>
2025-05-31 15:57:06 +0700

    Create Form Tambah Anggota

---
+++ page/anggota/anggota.php
@@ -0,0 +1,48 @@
+<div class="row">
+    <div class="col-md-12">
+        <!-- Advanced Tables -->
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                Data Anggota
+            </div>
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nisn</th>
+                                <th>Nama</th>
+                                <th>Tempat Lahir</th>
+                                <th>Tangal Lahir</th>
+                                <th>Jenis Kelamin</th>
+                                <th>Kelas</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                    <a href="" class="btn btn-info"><i class="fa fa-edit"></i>Ubah</a>
+                                    <a onclick="" href="" class="btn btn-danger"><i class="fa fa-trash"></i>Hapus</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <a href="" class="btn btn-primary" style="margin-top: 8px"><i class="fa fa-plus"></i>Tambah Data</a>
+
+                <a href="" target="blank" class="btn btn-default" style="margin-top : 8px"><i class="fa fa-print"></i>ExportToExel</a>
+
+
+            </div>
+        </div>
+    </div>
+</div>
---
+++ page/anggota/tambah.php
@@ -0,0 +1,55 @@
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        Tambah Data
+    </div>
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-md-12">
+                <form method="">
+                    <div class="form-group">
+                        <label>Nim</label>
+                        <input class="form-control" />
+
+                    </div>
+                    <div class="form-group">
+                        <label>Nama</label>
+                        <input class="form-control" />
+
+                    </div>
+                    <div class="form-group">
+                        <label>Tempat Lahir</label>
+                        <input class="form-control" />
+
+                    </div>
+                    <div class="form-group">
+                        <label>Tanggal Lahir</label>
+                        <input class="form-control" type="date" type />
+
+                    </div>
+                    <div class="form-group">
+                        <label>Jenis Kelamin</label> <br />
+                        <label class="checkbox-inline">
+                            <input type="checkbox" value="1" /> Laki-Laki
+                        </label>
+                        <label class="checkbox-inline">
+                            <input type="checkbox" value="2" /> Perempuan
+                        </label>
+
+                    </div>
+                    <div class="form-group">
+                        <label>Prodi</label>
+                        <input class="form-control" />
+
+                    </div>
+
+
+                    <div>
+                        <input type="submit" class="btn btn-primary">
+                    </div>
+
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
