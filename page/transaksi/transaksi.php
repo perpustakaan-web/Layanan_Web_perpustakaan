41e0d1eee450ed8f459d8437b45b9ba0266619d2
AminSolehudin <2203010110@unper.ac.id>
2025-06-12 15:35:26 +0700

    Create UI Transaksis

---
+++ page/transaksi/transaksi.php
@@ -0,0 +1,47 @@
+<a href="?page=transaksi&aksi=tambah" class="btn btn-primary" style="margin-bottom: 5px">Tambah Data</a>
+<div class="row">
+    <div class="col-md-12">
+        <!-- Advanced Tables -->
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                Data Transaksi
+            </div>
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Judul</th>
+                                <th>Nisn</th>
+                                <th>Nama</th>
+                                <th>Tanggal Pinjam</th>
+                                <th>Tanggal Kembali</th>
+                                <th>Terlambat</th>
+                                <th>Status</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+
+                                <td class="text-nowrap">
+                                    <a href="" class="btn btn-info">Kembali</a>
+                                    <a href="" class="btn btn-danger ">Perpanjang</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
