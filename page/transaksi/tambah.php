ad029c3014a1e22eec182c7735ac1100da4b0f42
AminSolehudin <2203010110@unper.ac.id>
2025-06-12 15:38:03 +0700

    Create UI Tambah Transaksi

---
+++ page/transaksi/tambah.php
@@ -0,0 +1,42 @@
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        Tambah Data
+    </div>
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-md-12">
+
+                <form method="">
+                    <div class="form-group">
+                        <label>Nama Buku</label>
+                        <select class="form-control" name="buku">
+
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Nisn || Nama Anggota</label>
+                        <select class="form-control" name="nama">
+
+                        </select>
+                    </div>
+                    <div>
+                        <label>Tanggal Pinjam</label>
+                        <input class="form-control" type="text" name="tgl_pinjam" id="tgl" readonly />
+                    </div>
+                    <div>
+                        <label>Tanggal Kembali</label>
+                        <input class="form-control" type="text" name="tgl_kembali" id="tgl" readonly />
+
+                    </div>
+
+                    <div>
+                        <input type="submit" name="simpan" value="Simpan" style="margin-top: 8px" class="btn btn-primary">
+                    </div>
+
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
